7c20731e834e87ba2816847ec54946839cfebcd1
John Carlo Nayan <nayanjohncarlo123@gmail.com>
2026-02-21 11:49:26 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nayan_Grade_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
